5f023ed9a9bdc7287983d0b7c679569c68d5a5dd
godwin-nj <amadigodwin7@gmail.com>
2023-09-12 14:32:48 +0100

    additional routes for upload of file into cloudinary

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,22 @@ const app = express();
 const cors = require("cors");
 const userRoute = require("./routes/user");
 const fileRouter = require("./routes/fileRoutes");
+const downLoadRouter = require("./routes/downLoadRouter");
 const fileUpload = require("express-fileupload");
+const { v2: cloudinary } = require("cloudinary");
 
 // middleware
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(cors());
-app.use(fileUpload());
-app.use(express.static("./public"));
+app.use(fileUpload({ useTempFiles: true }));
+
+// setting up cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 app.get("/", (req: Request, res: Response): void => {
   res.send("Hello Typescript with Node.js!");
@@ -19,6 +28,7 @@ app.get("/", (req: Request, res: Response): void => {
 // route middlware
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/upload", fileRouter);
+app.use("/api/v1/download", downLoadRouter);
 
 const PORT = process.env.PORT || 3000;
 
